25fce89c4d10196f09bb3b05aa1f493f420c114b
Sarthak160 <rocksarthak45@gmail.com>
2022-04-15 23:52:07 +0530

    gettcs and posttc toProtoTC

--- grpc/server/server.go
+++ grpc/server/server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/google/uuid"
 	"go.keploy.io/server/graph"
 	proto "go.keploy.io/server/grpc/regression"
+	"go.keploy.io/server/pkg/models"
 	regression2 "go.keploy.io/server/pkg/service/regression"
 	"go.keploy.io/server/pkg/service/run"
 	"go.uber.org/zap"
@@ -98,18 +99,7 @@ func helper(m map[string][]string) (res map[string]*proto.StrArr) {
 	}
 	return
 }
-
-func (srv *Server) GetTC(ctx context.Context, request *proto.GetTCRequest) (*proto.TestCase, error) {
-	id := request.Id
-	app := request.App
-	tcs, err := srv.svc.Get(ctx, graph.DEFAULT_COMPANY, app, id)
-	if err != nil {
-		return nil, err
-	}
-	tcs, err = srv.svc.Get(ctx, graph.DEFAULT_COMPANY, app, id)
-	if err != nil {
-		return nil, err
-	}
+func toProtoTC(tcs models.TestCase) (*proto.TestCase, error) {
 	reqHeader := helper(map[string][]string(tcs.HttpReq.Header))
 	respHeader := helper(map[string][]string(tcs.HttpResp.Header))
 	deps := []*proto.Dependency{}
@@ -129,7 +119,7 @@ func (srv *Server) GetTC(ctx context.Context, request *proto.GetTCRequest) (*pro
 			Data: data,
 		})
 	}
-	return &proto.TestCase{
+	ptcs := &proto.TestCase{
 		Id:       tcs.ID,
 		Created:  tcs.Created,
 		Updated:  tcs.Updated,
@@ -155,7 +145,73 @@ func (srv *Server) GetTC(ctx context.Context, request *proto.GetTCRequest) (*pro
 		AllKeys: allKeys,
 		Anchors: anchors,
 		Noise:   tcs.Noise,
-	}, nil
+	}
+	return ptcs, nil
+}
+
+func (srv *Server) GetTC(ctx context.Context, request *proto.GetTCRequest) (*proto.TestCase, error) {
+	id := request.Id
+	app := request.App
+	tcs, err := srv.svc.Get(ctx, graph.DEFAULT_COMPANY, app, id)
+	if err != nil {
+		return nil, err
+	}
+	// print(tcs)
+	tcs, err = srv.svc.Get(ctx, graph.DEFAULT_COMPANY, app, id)
+	if err != nil {
+		return nil, err
+	}
+	ptcs,err:= toProtoTC(tcs)
+	if err != nil {
+		return nil, err
+	}
+	return ptcs,nil
+	// reqHeader := helper(map[string][]string(tcs.HttpReq.Header))
+	// respHeader := helper(map[string][]string(tcs.HttpResp.Header))
+	// deps := []*proto.Dependency{}
+	// allKeys := helper(map[string][]string(tcs.AllKeys))
+	// anchors := helper(map[string][]string(tcs.Anchors))
+	// for _, j := range tcs.Deps {
+	// 	data := []*proto.DataBytes{}
+	// 	for _, k := range j.Data {
+	// 		data = append(data, &proto.DataBytes{
+	// 			Bin: k,
+	// 		})
+	// 	}
+	// 	deps = append(deps, &proto.Dependency{
+	// 		Name: j.Name,
+	// 		Type: string(j.Type),
+	// 		Meta: j.Meta,
+	// 		Data: data,
+	// 	})
+	// }
+	// return &proto.TestCase{
+	// 	Id:       tcs.ID,
+	// 	Created:  tcs.Created,
+	// 	Updated:  tcs.Updated,
+	// 	Captured: tcs.Captured,
+	// 	CID:      tcs.CID,
+	// 	AppID:    tcs.AppID,
+	// 	URI:      tcs.URI,
+	// 	HttpReq: &proto.HttpReq{
+	// 		Method:     string(tcs.HttpReq.Method),
+	// 		ProtoMajor: int64(tcs.HttpReq.ProtoMajor),
+	// 		ProtoMinor: int64(tcs.HttpReq.ProtoMinor),
+	// 		URL:        tcs.HttpReq.URL,
+	// 		URLParams:  tcs.HttpReq.URLParams,
+	// 		Header:     reqHeader,
+	// 		Body:       tcs.HttpReq.Body,
+	// 	},
+	// 	HttpResp: &proto.HttpResp{
+	// 		StatusCode: int64(tcs.HttpResp.StatusCode),
+	// 		Header:     respHeader,
+	// 		Body:       tcs.HttpResp.Body,
+	// 	},
+	// 	Deps:    deps,
+	// 	AllKeys: allKeys,
+	// 	Anchors: anchors,
+	// 	Noise:   tcs.Noise,
+	// }, nil
 
 }
 
@@ -164,8 +220,8 @@ func (srv *Server) GetTCS(ctx context.Context, request *proto.GetTCSRequest) (*p
 	if app == "" {
 		return nil, errors.New("app is required in request")
 	}
-	offsetStr:=request.Offset
-	limitStr:=request.Limit
+	offsetStr := request.Offset
+	limitStr := request.Limit
 	var (
 		offset int
 		limit  int
@@ -187,16 +243,36 @@ func (srv *Server) GetTCS(ctx context.Context, request *proto.GetTCSRequest) (*p
 	if err != nil {
 		return nil, err
 	}
-	print(tcs)
-	
-	return &proto.GetTCSResponse{Tcs: nil}, nil
+	var ptcs []*proto.TestCase
+	for i := 0; i < len(tcs); i++ {
+		ptc,err:= toProtoTC(tcs[i])
+		if err != nil {
+			return nil, err
+		}
+		ptcs = append(ptcs,ptc)
+	}
+
+	return &proto.GetTCSResponse{Tcs: ptcs}, nil
 }
 
-// func (srv *Server) PostTC(ctx context.Context, request *proto.TestCaseReq) (*proto.PostTCResponse, error)  {
-// 	// data:= &proto.TestCaseReq{
-// 	// 	Captured :request.Captured,
-// 	// 	AppID:request.AppID,
-// 	// 	URI:request.URI,
-// 	// 	HttpReq: request.HttpReq,
-// 	// }
-// }
+func (srv *Server) PostTC(ctx context.Context, request *proto.TestCaseReq) (*proto.PostTCResponse, error) {
+	data := &proto.TestCaseReq{
+		Captured:   request.Captured,
+		AppID:      request.AppID,
+		URI:        request.URI,
+		HttpReq:    request.HttpReq,
+		HttpResp:   request.HttpResp,
+		Dependency: request.Dependency,
+	}
+	now := time.Now().UTC().Unix()
+	var ptcs []*proto.TestCase
+	// for i := 0; i < len(tcs); i++ {
+	// 	ptc,err:= toProtoTC(tcs[i])
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	ptcs = append(ptcs,ptc)
+	// }
+	inserted, err := srv.svc.Put(ctx, graph.DEFAULT_COMPANY,[]*proto.TestCase )
+
+}
